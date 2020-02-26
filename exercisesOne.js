33d0098ed8ac4c56a21c3b9d1f07eb4755b5b360
Patrick Furtak <patrick@patrickfurtak.com>
2020-02-26 09:16:32 -0500

    finished family exercises

--- exercisesOne.js
+++ exercisesOne.js
@@ -1,22 +1,21 @@
 // Javascript Objects Exercises
 // Inheritance
 // Given the following objects:
-var mom = {
-  firstName: 'Alice',
-  lastName: 'Wong',
-  eyeColor: 'brown',
-  hairColor: 'black'
-};
+// var mom = {
+//   firstName: 'Alice',
+//   lastName: 'Wong',
+//   eyeColor: 'brown',
+//   hairColor: 'black'
+// };
 
-var daughter = {
-  firstName: 'Ilene',
-  hairColor: 'brown'
-};
+// var daughter = {
+//   firstName: 'Ilene',
+//   hairColor: 'brown'
+// };
 
 // One:
 // Write code to make daughter inherit properties from mom.
 // What are the daugther's properties and their values? Print them out.
-const momCSV = Object.values(mom).join(' ');
 
 class Bio {
   constructor(firstName, lastName, eyeColor, hairColor) {
@@ -25,9 +24,25 @@ class Bio {
     this.eyeColor = eyeColor;
     this.hairColor = hairColor;
   }
+  showInfo() {
+    console.log(`First name: ${this.firstName}`);
+    console.log(`Last Name: ${this.lastName}`);
+    console.log(`Eye color: ${this.eyeColor}`);
+    console.log(`Hair color: ${this.hairColor}`);
+  }
 }
 
-console.log(momCSV);
+class Child extends Bio {
+  constructor(firstName, lastName, eyeColor, hairColor) {
+    super(firstName, lastName, eyeColor, hairColor);
+  }
+}
+
+const mom = new Bio('Alice', 'Wong', 'Brown', 'Black');
+const daughter = new Child('Ilene', mom.lastName, mom.eyeColor, 'Brown');
+
+mom.showInfo();
+daughter.showInfo();
 
 // Two:
 // Add a method called showInfo to mom by attaching a function
